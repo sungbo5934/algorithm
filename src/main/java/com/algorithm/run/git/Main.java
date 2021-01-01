b3a301761f4bbc020adf8a0b9807a30dcbc89c11
sungb <sungb@DESKTOP-C0LDC34>
2021-01-01 13:04:50 +0900

    보석도둑 알고리즘 해결 우선순위 큐 학습 필요

--- src/main/java/com/algorithm/run/git/Main.java
+++ src/main/java/com/algorithm/run/git/Main.java
@@ -3,21 +3,14 @@ package com.algorithm.run.git;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.util.*;
 
 
 public class Main {
 	static int N;
 	static int K;
-	static List<Diamond> nInfo;
-	static List<Bag> kInfo;
-	static Boolean[] visited;
+	static Diamond[] nInfo;
+	static Integer[] kInfo;
 	static long totalPrice = 0;
 	static int giveCnt = 0;
 
@@ -28,87 +21,82 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
-		nInfo = new ArrayList<Diamond>();
-		kInfo = new ArrayList<Bag>();
+
+		nInfo = new Diamond[N];
+		kInfo = new Integer[K];
 		
 		for( int i = 0 ; i < N ; i++ ) {
 			st = new StringTokenizer(br.readLine());
-			nInfo.add(new Diamond(i ,Integer.parseInt(st.nextToken()) , Integer.parseInt(st.nextToken())));
+			nInfo[i] = new Diamond(Integer.parseInt(st.nextToken()) , Integer.parseInt(st.nextToken()));
 		}
 		
 		for( int i = 0 ; i < K ; i++ ) {
 			st = new StringTokenizer(br.readLine());
-			kInfo.add(new Bag(Integer.parseInt(st.nextToken())));
+			kInfo[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		//Collections.sort(nInfo, new ComparatorDia());
-		Collections.sort(kInfo, new ComparatorBag());
+		Arrays.sort(nInfo,  new ComparatorDiaWit());
+		Arrays.sort(kInfo);
 
-		/*
-		 * for(Diamond dia : nInfo) { if(giveCnt < K ) { for(int i = 0 ; i <
-		 * kInfo.size() ; i++) { if(dia.getWeight() <= kInfo.get(i).getLimit()) {
-		 * totalPrice += dia.getPrice(); giveCnt++; kInfo.remove(i); break; } } }else {
-		 * break; } }
-		 */
-		
-//		for(Bag bag : kInfo) {
-//			if(giveCnt < K ) {
-//				for(int i=0 ; i < nInfo.size() ; i++) {
-//					if(nInfo.get(i).getWeight() <= bag.getLimit()) {
-//						totalPrice += nInfo.get(i).getPrice();
-//						giveCnt++;
-//						nInfo.remove(i);
-//						break;
-//					}
-//				}
-//			}else {
-//				break;
-//			}
-//		}
-		for(Bag bag : kInfo) {
-			if(giveCnt < K ) {
-				List<Diamond> list = nInfo.stream().filter(data -> data.getWeight() <= bag.getLimit()).collect(Collectors.toList());
-				if(list != null) {
-					Collections.sort(list, new ComparatorDia());
-					for(int i=0 ; i < list.size() ; i++) {
-						if(list.get(i).getWeight() <= bag.getLimit()) {
-							totalPrice += list.get(i).getPrice();
-							giveCnt++;
-							nInfo.remove(list.get(i));
-							break;
-						}
-					}
-				}
-			}else {
-				break;
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		int index = 0;
+		for(int i : kInfo) {
+			while(index < N && nInfo[index].weight <= i) {
+				pq.add(nInfo[index].price);
+				index++;
 			}
+			if(!pq.isEmpty()) {
+				totalPrice += pq.poll();
+			}
+
 		}
 		
 		System.out.println(totalPrice);
 
+	}
+	static class Diaindex implements Comparable<Diaindex> {
+		int index;
+		int weight;
+		int price;
+		
+		public Diaindex(int index ,int weight,  int price) {
+			this.index =index;
+			this.weight =  weight;
+			this.price = price;
+		}
+
+		@Override
+		public int compareTo(Diaindex o) {
+			 if (this.price < o.price) {
+				 return 1;
+			 }else if(this.price > o.price) {
+				 return -1;
+			 }else{
+				 if(this.weight < o.weight) {
+					 return -1;
+				 }else if(this.weight > o.weight){
+					 return 1;
+				 }else {
+					 return 0;
+				 }
+			 }
+		}
+		
 	}
 	
 	static class Diamond{
 		int index;
 		int weight;
 		int price;
-		public Diamond(int index ,int weight , int price) {
-			this.index = index;
+		
+		public Diamond( int weight , int price) {
 			this.weight = weight;
 			this.price = price;
 		}
 		
-		public int getIndex(){
-			return this.index;
-		}
-		
-		public int getWeight(){
-			return this.weight;
-		}
-		
-		public int getPrice(){
-			return this.price;
+		public Diamond(int index , int weight , int price) {
+			this.index =index;
+			this.weight = weight;
+			this.price = price;
 		}
 		
 		
@@ -128,12 +116,28 @@ public class Main {
 	static class ComparatorDia implements Comparator<Diamond>{
 		
 		public int compare(Diamond o1, Diamond o2) {
-			int firstPrice = o1.getPrice();
-			int secondPrice = o2.getPrice();
+			int firstPrice = o1.price;
+			int secondPrice = o2.price;
 			if (firstPrice > secondPrice) {
 				return -1;
 			}else if(firstPrice < secondPrice) {
 				return 1;
+			}else {
+				return 0;
+			}
+		}
+		
+	}
+	
+	static class ComparatorDiaWit implements Comparator<Diamond>{
+		
+		public int compare(Diamond o1, Diamond o2) {
+			int firstPrice = o1.weight;
+			int secondPrice = o2.weight;
+			if (firstPrice > secondPrice) {
+				return 1;
+			}else if(firstPrice < secondPrice) {
+				return -1;
 			}else {
 				return 0;
 			}
@@ -158,3 +162,4 @@ public class Main {
 		}
 
 }
+
