5b197255d957d6c97ab87433eaba77d324d95644
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-08-23 17:24:37 +0900

    백준

---
+++ src/main/java/com/algorithm/run/backjun/SortingDeepArray2.java
@@ -0,0 +1,47 @@
+package com.algorithm.run.backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortingDeepArray2 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Integer[][] arr =  new Integer[n][2];
+		for(int i = 0 ; i < n ; i++) {
+			String str = br.readLine();
+			arr[i][0] = Integer.parseInt(str.split(" ")[0]);
+			arr[i][1] = Integer.parseInt(str.split(" ")[1]);
+		}
+		
+		Arrays.sort(arr, new Comparator<Integer[]>() {
+
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				if(o1[1] > o2[1]) {
+					return 1;
+				}else if(o1[1] < o2[1]) {
+					return -1;
+				}else {
+					if(o1[0] > o2[0]) {
+						return 1;
+					}else if(o1[0] < o2[0]) {
+						return -1;
+					}else {
+						return 0;
+					}
+				}
+			}
+		});
+		
+		for(int i = 0 ; i < n ; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+
+	}
+
+}
---
+++ src/main/java/com/algorithm/run/backjun/SortingWord.java
@@ -0,0 +1,55 @@
+package com.algorithm.run.backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SortingWord {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Set<String> set = new HashSet<String>();
+		for(int i = 0 ; i < n ; i++) {
+			String str = br.readLine();
+			set.add(str);
+		}
+		List<String> list = set.stream().sorted(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length() < o2.length()) {
+					return -1;
+				}else if(o1.length() > o2.length()) {
+					return 1;
+				}else {
+					return o1.compareTo(o2);
+				}
+			}
+		}).collect(Collectors.toList());
+		
+		for(int i = 0 ; i < list.size() ; i++) {
+			System.out.println(list.get(i));
+		}
+
+	}
+
+}
+/**
+ * 
+13
+but
+i
+wont
+hesitate
+no
+more
+no
+more
+it
+cannot
+wait
+im
+yours
+ */
