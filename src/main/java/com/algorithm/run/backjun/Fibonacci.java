02b8d4014dcee8f48fb3eb991799b96634da6803
sungb <sungb@DESKTOP-C0LDC34>
2021-08-23 21:52:38 +0900

    백준

---
+++ src/main/java/com/algorithm/run/backjun/Fibonacci.java
@@ -0,0 +1,34 @@
+package com.algorithm.run.backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] answerArr = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			int num = Integer.parseInt(br.readLine());
+			answerArr[i] = num;
+		}
+		
+		int max = Arrays.stream(answerArr).max().getAsInt();
+		String[] dp = new String[max + 1]; 
+		dp[0] = "1,0";
+		dp[1] = "0,1";
+		for(int i = 2 ; i <= max ; i++) {
+			dp[i] = (Integer.parseInt(dp[i -1].split(",")[0]) + Integer.parseInt(dp[i -2].split(",")[0])) +"," + (Integer.parseInt(dp[i -1].split(",")[1]) + Integer.parseInt(dp[i -2].split(",")[1]));
+		}
+		
+		for(int i = 0 ; i < n ; i++) {
+			System.out.println(dp[answerArr[i]].split(",")[0] + " " + dp[answerArr[i]].split(",")[1]);
+		}
+
+	}
+
+
+}
