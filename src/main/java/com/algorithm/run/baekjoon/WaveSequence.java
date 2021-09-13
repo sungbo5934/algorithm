fdec9e33b0630fb80d1eaf3c6aef98d5fe019f65
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-09-13 15:13:41 +0900

    파도 나선형 백준

---
+++ src/main/java/com/algorithm/run/baekjoon/WaveSequence.java
@@ -0,0 +1,15 @@
+package com.algorithm.run.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WaveSequence {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+
+	}
+
+}
