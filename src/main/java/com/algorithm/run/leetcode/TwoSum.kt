d7f499e4b80f19d601f84fd61407b9c45183b77e
sum.sb <sum.sb@kakaoban.com>
2022-06-09 10:41:20 +0900

    투썸플레이슨

--- src/main/java/com/algorithm/run/leetcode/TwoSum.kt
+++ src/main/java/com/algorithm/run/leetcode/TwoSum.kt
@@ -5,6 +5,16 @@ package com.algorithm.run.leetcode
  */
 class TwoSum {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        return intArrayOf()
+        var intArray: IntArray =  intArrayOf()
+        loop1@ for( i in nums.indices){
+            val first = nums[i]
+            for( j in i+1 until nums.size){
+                if(nums[j] == target - first) {
+                    intArray =  intArrayOf(i,j)
+                    break@loop1
+                }
+            }
+        }
+        return intArray
     }
 }
